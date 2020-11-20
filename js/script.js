66458a4c9e6825ba6c54e833f72fe6dfd9b02557
Kelli Rainer <kellirainer@Kellis-MBP.lan1>
2020-11-20 13:39:51 -0500

    f

---
+++ js/script.js
@@ -0,0 +1,119 @@
+function alert1() {
+  alert(
+    "In 1776, the Founding Fathers establish voting rights and procedures in the United States. They exclude women, slaves, immigrants and many other minority groups. The only people allowed to vote were  landowners, most of whom are white, Protestant males over age 21. This foundational law will lead to centuries of minority groups fighting for their right to participate in democracy."
+  );
+}
+
+function alert2() {
+  alert(
+    "The Founding Fathers establish voting rights and procedures in the United States. They exclude women, slaves, immigrants and many other minority groups. The only people allowed to vote were  landowners, most of whom are white, Protestant males over age 21. This foundational law will lead to centuries of minority groups fighting for their right to participate in democracy."
+  );
+}
+
+function alert3() {
+  alert(
+    "The Founding Fathers establish voting rights and procedures in the United States. They exclude women, slaves, immigrants and many other minority groups. The only people allowed to vote were  landowners, most of whom are white, Protestant males over age 21. This foundational law will lead to centuries of minority groups fighting for their right to participate in democracy."
+  );
+}
+
+function alert4() {
+  alert(
+    "The Founding Fathers establish voting rights and procedures in the United States. They exclude women, slaves, immigrants and many other minority groups. The only people allowed to vote were  landowners, most of whom are white, Protestant males over age 21. This foundational law will lead to centuries of minority groups fighting for their right to participate in democracy."
+  );
+}
+
+function alert5() {
+  alert(
+    "The Founding Fathers establish voting rights and procedures in the United States. They exclude women, slaves, immigrants and many other minority groups. The only people allowed to vote were  landowners, most of whom are white, Protestant males over age 21. This foundational law will lead to centuries of minority groups fighting for their right to participate in democracy."
+  );
+}
+
+function alert6() {
+  alert(
+    "The Founding Fathers establish voting rights and procedures in the United States. They exclude women, slaves, immigrants and many other minority groups. The only people allowed to vote were  landowners, most of whom are white, Protestant males over age 21. This foundational law will lead to centuries of minority groups fighting for their right to participate in democracy."
+  );
+}
+
+function alert7() {
+  alert(
+    "The Founding Fathers establish voting rights and procedures in the United States. They exclude women, slaves, immigrants and many other minority groups. The only people allowed to vote were  landowners, most of whom are white, Protestant males over age 21. This foundational law will lead to centuries of minority groups fighting for their right to participate in democracy."
+  );
+}
+
+function alert8() {
+  alert(
+    "The Founding Fathers establish voting rights and procedures in the United States. They exclude women, slaves, immigrants and many other minority groups. The only people allowed to vote were  landowners, most of whom are white, Protestant males over age 21. This foundational law will lead to centuries of minority groups fighting for their right to participate in democracy."
+  );
+}
+
+function alert9() {
+  alert(
+    "The Founding Fathers establish voting rights and procedures in the United States. They exclude women, slaves, immigrants and many other minority groups. The only people allowed to vote were  landowners, most of whom are white, Protestant males over age 21. This foundational law will lead to centuries of minority groups fighting for their right to participate in democracy."
+  );
+}
+
+function alert10() {
+  alert(
+    "The Founding Fathers establish voting rights and procedures in the United States. They exclude women, slaves, immigrants and many other minority groups. The only people allowed to vote were  landowners, most of whom are white, Protestant males over age 21. This foundational law will lead to centuries of minority groups fighting for their right to participate in democracy."
+  );
+}
+
+function alert11() {
+  alert(
+    "The Founding Fathers establish voting rights and procedures in the United States. They exclude women, slaves, immigrants and many other minority groups. The only people allowed to vote were  landowners, most of whom are white, Protestant males over age 21. This foundational law will lead to centuries of minority groups fighting for their right to participate in democracy."
+  );
+}
+
+function alert12() {
+  alert(
+    "The Founding Fathers establish voting rights and procedures in the United States. They exclude women, slaves, immigrants and many other minority groups. The only people allowed to vote were  landowners, most of whom are white, Protestant males over age 21. This foundational law will lead to centuries of minority groups fighting for their right to participate in democracy."
+  );
+}
+
+function alert13() {
+  alert(
+    "The Founding Fathers establish voting rights and procedures in the United States. They exclude women, slaves, immigrants and many other minority groups. The only people allowed to vote were  landowners, most of whom are white, Protestant males over age 21. This foundational law will lead to centuries of minority groups fighting for their right to participate in democracy."
+  );
+}
+
+function alert14() {
+  alert(
+    "The Founding Fathers establish voting rights and procedures in the United States. They exclude women, slaves, immigrants and many other minority groups. The only people allowed to vote were  landowners, most of whom are white, Protestant males over age 21. This foundational law will lead to centuries of minority groups fighting for their right to participate in democracy."
+  );
+}
+
+function alert15() {
+  alert(
+    "The Founding Fathers establish voting rights and procedures in the United States. They exclude women, slaves, immigrants and many other minority groups. The only people allowed to vote were  landowners, most of whom are white, Protestant males over age 21. This foundational law will lead to centuries of minority groups fighting for their right to participate in democracy."
+  );
+}
+
+function alert16() {
+  alert(
+    "The Founding Fathers establish voting rights and procedures in the United States. They exclude women, slaves, immigrants and many other minority groups. The only people allowed to vote were  landowners, most of whom are white, Protestant males over age 21. This foundational law will lead to centuries of minority groups fighting for their right to participate in democracy."
+  );
+}
+
+function alert17() {
+  alert(
+    "The Founding Fathers establish voting rights and procedures in the United States. They exclude women, slaves, immigrants and many other minority groups. The only people allowed to vote were  landowners, most of whom are white, Protestant males over age 21. This foundational law will lead to centuries of minority groups fighting for their right to participate in democracy."
+  );
+}
+
+function alert18() {
+  alert(
+    "The Founding Fathers establish voting rights and procedures in the United States. They exclude women, slaves, immigrants and many other minority groups. The only people allowed to vote were  landowners, most of whom are white, Protestant males over age 21. This foundational law will lead to centuries of minority groups fighting for their right to participate in democracy."
+  );
+}
+
+function alert19() {
+  alert(
+    "The Founding Fathers establish voting rights and procedures in the United States. They exclude women, slaves, immigrants and many other minority groups. The only people allowed to vote were  landowners, most of whom are white, Protestant males over age 21. This foundational law will lead to centuries of minority groups fighting for their right to participate in democracy."
+  );
+}
+
+function alert20() {
+  alert(
+    "The Founding Fathers establish voting rights and procedures in the United States. They exclude women, slaves, immigrants and many other minority groups. The only people allowed to vote were  landowners, most of whom are white, Protestant males over age 21. This foundational law will lead to centuries of minority groups fighting for their right to participate in democracy."
+  );
+}
