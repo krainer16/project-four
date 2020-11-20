eb58cca2c2f0e44471ff1356e75d5b21650030ba
Kelli Rainer <kellirainer@Kellis-MBP.lan1>
2020-11-20 15:57:47 -0500

    e

--- js/script.js
+++ js/script.js
@@ -6,25 +6,25 @@ function alert1() {
 
 function alert2() {
   alert(
-    "The Founding Fathers establish voting rights and procedures in the United States. They exclude women, slaves, immigrants and many other minority groups. The only people allowed to vote were  landowners, most of whom are white, Protestant males over age 21. This foundational law will lead to centuries of minority groups fighting for their right to participate in democracy."
+    "In 1787, the standard for federal voting was revoked, and now states are permitted to determine their own voting policies within their borders. Since there are no longer federal mandates on voting procedures, many states keep the voting privileges only within white, male landowning citizens still."
   );
 }
 
 function alert3() {
   alert(
-    "The Founding Fathers establish voting rights and procedures in the United States. They exclude women, slaves, immigrants and many other minority groups. The only people allowed to vote were  landowners, most of whom are white, Protestant males over age 21. This foundational law will lead to centuries of minority groups fighting for their right to participate in democracy."
+    "1848 marked a change in citizenship laws, which consequently caused alterations in voting policies. The Treaty of Guadalupe-Hidalgo was signed, which terminated the Mexican-American War and granted citizenship to Mexican citizens who lived in the newly conquered American lands. Due to English proficiency language requirements, however, these new citizens struggled to access their voting privileges."
   );
 }
 
 function alert4() {
   alert(
-    "The Founding Fathers establish voting rights and procedures in the United States. They exclude women, slaves, immigrants and many other minority groups. The only people allowed to vote were  landowners, most of whom are white, Protestant males over age 21. This foundational law will lead to centuries of minority groups fighting for their right to participate in democracy."
+    "Original state laws from the 1700’s largely limited voting rights to white men who were also landowners, but states slowly start to repeal this law and open up voting privileges to white men, regardless of landowning and financial status. In 1856, North Carolina is the last state to remove this restriction, so now all American white men are eligible to vote.    "
   );
 }
 
 function alert5() {
   alert(
-    "The Founding Fathers establish voting rights and procedures in the United States. They exclude women, slaves, immigrants and many other minority groups. The only people allowed to vote were  landowners, most of whom are white, Protestant males over age 21. This foundational law will lead to centuries of minority groups fighting for their right to participate in democracy."
+    "TIn 1868, the Civil War is over, and the Emancipation Proclamation has been released. With the passing of the 14th Amendment, former slaves are now free in the United States, and they are granted citizenship finally. Voters are still primarily male, and voting rights are primarily decided by the states still, despite federal regulation. As a result, many people are still refused their basic right to participate in democracy."
   );
 }
 
