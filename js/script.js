728b43b99976f2a918b495e9ff56bd40f173ad4e
Kelli Rainer <kellirainer@Kellis-MBP.lan1>
2020-11-20 21:19:40 -0500

    y

--- js/script.js
+++ js/script.js
@@ -117,3 +117,29 @@ function alert20() {
     "In 2001, the debate surrounding the voting rights of felons becomes a hot topic of discussion on the political front. Around four million American citizens are unable to vote due to prior criminal charges. The National Commission on Federal Election Reform announces its recommendation for states to permit felons to vote once their sentences are completed."
   );
 }
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
